abb445946ce50ca8896187b960538108fbe6a27d
zeeshaan <zeeshaan.a@gmail.com>
2020-11-26 16:45:20 +0530

    f4

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args () ){  
-     System.out.println("Hello ZeeClasses Tanda AN ");  
+     System.out.println("Hello ZeeClasses Tanda AN UP ");  
     }  
 }  
