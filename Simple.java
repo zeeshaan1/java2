8461fdcd5434e3cac2e2803e7c48f1c260fb041b
zeeshaan <zeeshaan.a@gmail.com>
2020-11-26 15:09:38 +0530

    reinitialized files

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args () ){  
-     System.out.println("Hello ZeeClasses India Asia World");  
+     System.out.println("Hello ZeeClasses Indira");  
     }  
 }  
