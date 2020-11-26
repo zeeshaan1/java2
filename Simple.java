57b21ee63330a08e9cd91c7f9e32c52b3a542157
zeeshaan <zeeshaan.a@gmail.com>
2020-11-26 15:30:52 +0530

    f3

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args () ){  
-     System.out.println("Hello ZeeClasses Indirapuram");  
+     System.out.println("Hello ZeeClasses Tanda");  
     }  
 }  
